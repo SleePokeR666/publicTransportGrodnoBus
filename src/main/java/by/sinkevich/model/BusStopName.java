8e4a7df321d62eb197b361ca2c1ab3eea0631cb6
Denis Sinkevich <sleepoker@gmail.com>
2018-12-01 21:46:37 +0300

    Refactor BusStopName enum
    Add different values for maxBusCapacity, routeTime and
    passengersLoadTime.

--- src/main/java/by/sinkevich/model/BusStopName.java
+++ src/main/java/by/sinkevich/model/BusStopName.java
@@ -2,17 +2,41 @@ package by.sinkevich.model;
 
 public enum BusStopName {
 
-	SOVETSKAYA,
-	YANKI_KUPALI,
-	VISHNEVETS,
-	KORONA_MARKET,
-	ZARITSA,
-	TOMINA,
-	OLSHANKA,
-	GORKOGO,
-	DOMBROVSKOGO,
-	OBUVNAYA_FABRIKA,
-	FOLUSH,
-	TRAIN_STATION,
-	DEVIATOVKA
+	SOVETSKAYA(3, 600, 1000),
+	YANKI_KUPALI(2, 500, 1250),
+	VISHNEVETS(4, 400, 1500),
+	KORONA_MARKET(2, 500, 1250),
+	ZARITSA(4, 400, 1500),
+	TOMINA(2, 500, 1250),
+	OLSHANKA(4, 400, 1500),
+	GORKOGO(2, 500, 1250),
+	DOMBROVSKOGO(4, 400, 1500),
+	OBUVNAYA_FABRIKA(2, 500, 1250),
+	FOLUSH(4, 400, 1500),
+	TRAIN_STATION(2, 500, 1250),
+	DEVIATOVKA(4, 400, 1500);
+
+	private int maxBusCapacity;
+
+	private long routeTime;
+
+	private long passengersLoadTime;
+
+	BusStopName(int maxBusCapacity, long routeTime, long passengersLoadTime) {
+		this.maxBusCapacity = maxBusCapacity;
+		this.routeTime = routeTime;
+		this.passengersLoadTime = passengersLoadTime;
+	}
+
+	public int getMaxBusCapacity() {
+		return maxBusCapacity;
+	}
+
+	public long getRouteTime() {
+		return routeTime;
+	}
+
+	public long getPassengersLoadTime() {
+		return passengersLoadTime;
+	}
 }
